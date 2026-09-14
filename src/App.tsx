972d1cd6bb63fbca6f89e55ec41c80c43020e016
Gerencsér János <gerencserjanos1017@gmail.com>
2026-09-14 12:08:18 +0200

    test: verify branch protection setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -113,10 +113,10 @@ function App() {
         </div>
       </section>
 
-      <div className="ticks"></div>
+      <div className="ticks">Ticks</div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
